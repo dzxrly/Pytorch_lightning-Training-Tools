5c86477a32247067edc70e7dc73bd130b1a70128
Egg Targaryen <dzxrly2@gmail.com>
2024-04-22 15:58:37 +0800

    :heavy_plus_sign: 使用rich的print替换基础print

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 - pytorch_lightning
 - torch
 - torchinfo
+- rich
 
 ## Install Dependences
 
--- tools.py
+++ tools.py
@@ -6,16 +6,8 @@ import numpy as np
 import pynvml
 import pytorch_lightning as pl
 import torch
+from rich import print
 from torchinfo import summary
-
-
-def set_random_seed(random_seed: Union[float, int]) -> None:
-    torch.manual_seed(random_seed)
-    torch.cuda.manual_seed(random_seed)
-    torch.cuda.manual_seed_all(random_seed)
-    np.random.seed(random_seed)
-    random.seed(random_seed)
-    pl.seed_everything(random_seed)
 
 
 def convert_str_params_list(str_params: str, params_type: str, split_marker: str = '#') -> list:
@@ -24,33 +16,42 @@ def convert_str_params_list(str_params: str, params_type: str, split_marker: str
     return params_list
 
 
+def set_random_seed(random_seed: Union[float, int]):
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.cuda.manual_seed_all(random_seed)
+    np.random.seed(random_seed)
+    random.seed(random_seed)
+    pl.seed_everything(random_seed)
+
+
 def predict_model_memory_usage(
         model: torch.nn.Module,
         input_shape: list[...],
-        batch_size: int,
-        model_dtype: torch.dtype = torch.bfloat16,
-        input_dtypes: list[...] = None,
-        default_device: torch.device = torch.device('cuda:0'),
+        input_dtypes=None,
+        verbose: bool = True,
 ) -> float:
     """
     calculate model memory usage
     :param model: model
     :param input_shape: input shape
-    :param batch_size: batch size
-    :param model_dtype: model dtype
-    :param input_dtypes: input dtypes
-    :param default_device: default device
+    :param input_dtypes: list of input data type, default is [torch.float32]
+    :param verbose: if True, print model memory usage
     :return: predicted memory usage (GB)
     """
     if input_dtypes is None:
-        input_dtypes = [torch.bfloat16, torch.bfloat16]
-    input_tensor = torch.randn(batch_size, *input_shape)
-    summary_info = summary(model.to(default_device, dtype=model_dtype), input_size=input_tensor.shape,
-                           device=default_device, mode='train', dtypes=input_dtypes)
+        input_dtypes = [torch.float32] * len(input_shape)
+    assert len(input_shape) == len(input_dtypes), '[Error] input_shape and input_dtypes must have same length'
+    summary_info = summary(
+        model,
+        input_size=input_shape,
+        device=torch.device('cpu'),
+        mode='train',
+        dtypes=input_dtypes,
+        verbose=verbose,
+    )
     memory_usage = summary_info.total_input + summary_info.total_output_bytes + summary_info.total_param_bytes
     # detach input tensor and model, then release memory
-    torch.cuda.empty_cache()
-    del input_tensor
     del model
     del summary_info
     return memory_usage / 1024 / 1024 / 1024
@@ -61,6 +62,7 @@ def auto_find_memory_free_card(
         model_memory_usage: float,
         idle: bool = False,
         idle_max_seconds: int = 60 * 60 * 24,
+        verbose: bool = True,
 ) -> int:
     """
     auto find memory free card
@@ -68,6 +70,7 @@ def auto_find_memory_free_card(
     :param model_memory_usage: model memory usage (GB)
     :param idle: if True, waiting until there is a card with free memory
     :param idle_max_seconds: max waiting seconds
+    :param verbose: if True, print logs
     :return: card id
     """
     device_count = torch.cuda.device_count()
@@ -75,8 +78,9 @@ def auto_find_memory_free_card(
     pynvml.nvmlInit()
     if idle:
         start_time = time.time()
-        print('[Info] waiting for idle card, waiting begin time:',
-              time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time)))
+        if verbose:
+            print('[Info] waiting for idle card, waiting begin time:',
+                  time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time)))
         while True:
             if time.time() - start_time > idle_max_seconds:
                 # shutdown pynvml
@@ -84,38 +88,43 @@ def auto_find_memory_free_card(
                 raise TimeoutError(
                     f'[Error] no card has enough memory to load model, model memory usage is {model_memory_usage} MB')
             # get all card free memory and use the card with max free memory
-            print('=' * 50)
-            print('[Info] waiting for idle card, waiting time: {:.0f} seconds'.format(time.time() - start_time))
+            if verbose:
+                print('=' * 50)
+                print('[Info] waiting for idle card, waiting time: {:.0f} seconds'.format(time.time() - start_time))
             free_memory_list = []
             for card_id in card_list:
                 # get card free memory by pynvml
                 handle = pynvml.nvmlDeviceGetHandleByIndex(card_id)
                 info = pynvml.nvmlDeviceGetMemoryInfo(handle)
                 free_memory = info.free / 1024 / 1024 / 1024
-                print('[Info] cuda:{}, free memory: {:.1f} GB, model needs: {:.1f} GB'.format(
-                    card_id,
-                    free_memory,
-                    model_memory_usage,
-                ))
+                if verbose:
+                    print('[Info] cuda:{}, free memory: {:.1f} GB, model needs: {:.1f} GB'.format(
+                        card_id,
+                        free_memory,
+                        model_memory_usage,
+                    ))
                 if free_memory > model_memory_usage:
                     free_memory_list.append({
                         'card_id': card_id,
                         'free_memory': free_memory,
                     })
-            print('=' * 50)
+            if verbose:
+                print('=' * 50)
             if len(free_memory_list) != 0:
                 # shutdown pynvml
                 pynvml.nvmlShutdown()
                 # sort free memory list by free memory descending
                 free_memory_list.sort(key=lambda x: x['free_memory'], reverse=True)
                 max_free_memory_card_id = free_memory_list[0]['card_id']
-                print('[Info] find free card cuda:{}, training begin time: {}'.format(
-                    max_free_memory_card_id,
-                    time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
-                )
+                if verbose:
+                    print('[Info] find free card cuda:{}, training begin time: {}'.format(
+                        max_free_memory_card_id,
+                        time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+                    )
                 return max_free_memory_card_id
             else:
-                print('[Info] no card has enough memory to load model, waiting for 60 seconds')
+                if verbose:
+                    print('[Info] no card has enough memory to load model, waiting for 60 seconds')
                 time.sleep(60)
 
     else:
